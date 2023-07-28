b514b784f6ebb33040150e087fa787cedfca6528
paco del valle <pacodelvalle@MacBook-Air-de-Gonzalo.local>
2023-07-28 14:58:03 +0200

    added menu and change css

--- audio/script.js
+++ audio/script.js
@@ -1,4 +1,17 @@
-function abrirVentanaAudio() {
+
     // Open new window with the sound
+function abrirVentanaAudio() {
     window.open("audio.html", "VentanaAudio", "width=200,height=100");
+}
+
+//funcion para desplegar el menu en el index
+function openMenu() {
+    var menu = document.getElementById('menu');
+    menu.classList.toggle('show');
+}
+
+//funcion para cerrar el memu en el index
+function closeMenu() {
+    var menu = document.getElementById('menu');
+    menu.classList.remove('show');
 }
